d9839714ca4b0c89b4059ee52882e13ac81c2885
Josh Geisler <joshuageisler@gmail.com>
2020-07-08 18:55:35 -0400

    passed

--- actions.js
+++ actions.js
@@ -4,4 +4,42 @@ const axios = require('axios');
 const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require('./types');
 
 /* YOUR CODE GOES HERE */
+
+const gotDesserts = (arr) => {
+    return {
+        type: GOT_DESSERTS_DATA,
+        desserts: arr
+    }
+}
+
+const addDessert = (arr) => {
+    return {
+        type: NEW_DESSERT_DATA,
+        newDessert: arr
+    }
+}
+
+const fetchDesserts = () => {
+    return (dispatch) => {
+        return new Promise((res, rej) => {
+            axios.get('/api/desserts')
+                .then(res => dispatch(gotDesserts(res.data.desserts)))
+                .then(() => res())
+                .catch(e => rej(e))
+        })
+    }
+}
+
+const postDessert = () => {
+    return (dispatch) => {
+        return new Promise((res, rej) => {
+            axios.post('/api/desserts')
+                .then(res => dispatch(addDessert(res.data.newDessert)))
+                .then(() => res())
+                .catch(e => rej(e))
+
+        })
+    }
+}
+
 module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
